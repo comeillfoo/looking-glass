cb6934ba5d15e7526a439981069f224700ee3414
Come1LLF00 <rayveleaveme34@inbox.ru>
2021-12-10 02:11:40 +0300

    init: vue.js project

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <Login />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+  import { defineComponent } from 'vue';
+  import Login from './components/Login.vue';
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
+  export default defineComponent({
+    name: 'App',
+    components: {
+      Login
+    }
+  });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  *, *:after, *:before {
+    box-sizing: border-box;
+  }
+
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  .box {
+    display: inline-block;
+    padding: .8rem;
+    border: solid 2px dimgray;
+  }
+
+  .box-rounded {
+    border-radius: .4rem;
+  }
+
 </style>
---
+++ src/components/Login.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class='box box-rounded'>
+    <h3>Вход</h3>
+    <form @submit.prevent='login'>
+      <fieldset>
+        <label>ИД</label><input type='number' placeholder='4020' v-model='id' required />
+      </fieldset>
+      <fieldset>
+        <input type='submit' value='войти' />
+        <input type='reset' value='сбросить' />
+      </fieldset>
+    </form>
+  </div>
+</template>
+
+<script lang='ts'>
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: 'Login',
+
+    props: {
+      queryLogin: {
+        type: String,
+        default: '/alice/api/get-resident-by-id',
+      },
+    },
+
+    data() {
+      return { id: null }; 
+    },
+
+    methods: {
+      login: async function( submit_event: Event ) {
+        submit_event.preventDefault();
+        console.log( `login:${this.id}` );
+        let login_response = await fetch( `${this.queryLogin}/${this.id}`, { method: 'GET' } );
+        console.log( login_response );
+        if ( login_response.status == 200 ) {
+          let user = await login_response.json();
+          console.log( user );
+        }
+      },
+    }
+  });
+</script>
+
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
+<style scoped>
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+</style>
